c280f51b83198db53ed982f78c8da43c39782d18
ilearn-code <satyamyadavv931@gmail.com>
2023-06-23 00:18:39 +0530

    added user for final viva

--- chat_db.sql
+++ chat_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2023 at 01:27 PM
+-- Generation Time: Jun 22, 2023 at 08:47 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -35,31 +35,6 @@ CREATE TABLE `chat_messages` (
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `chat_messages`
---
-
-INSERT INTO `chat_messages` (`id`, `sender_id`, `receiver_id`, `message`, `created_at`) VALUES
-(360, 9, 4, 'hi', '2023-06-05 19:47:06'),
-(361, 81761352, 4, 'jbjb', '2023-06-05 19:47:31'),
-(362, 81761352, 9, 'gvgcv', '2023-06-05 19:47:39'),
-(363, 81761352, 9, 'hi', '2023-06-05 19:55:58'),
-(364, 81761352, 52820522, 'hi', '2023-06-05 19:58:00'),
-(365, 81761352, 81761354, 'what are u doin', '2023-06-05 19:58:06'),
-(366, 81761352, 81761354, 'nopie', '2023-06-05 20:00:04'),
-(367, 81761352, 81761354, 'yoo', '2023-06-05 20:08:48'),
-(368, 81761352, 81761354, 'hjh', '2023-06-05 20:23:21'),
-(369, 81761352, 81761354, 'njnj', '2023-06-05 20:23:50'),
-(370, 81761352, 81761354, 'bbhbh', '2023-06-05 20:23:52'),
-(371, 81761352, 81761354, 'bghbh', '2023-06-05 20:23:53'),
-(372, 81761352, 81761354, 'gvgvg', '2023-06-05 20:23:55'),
-(373, 81761352, 81761354, 'bvhvh', '2023-06-05 20:23:56'),
-(374, 81761352, 81761354, 'hvh', '2023-06-05 20:23:57'),
-(375, 81761352, 81761354, 'bbhj', '2023-06-05 20:23:58'),
-(376, 81761352, 81761354, 'hv', '2023-06-05 20:23:59'),
-(377, 81761352, 81761354, '  bn', '2023-06-05 20:24:01'),
-(378, 81761352, 81761354, 'b vh', '2023-06-05 20:24:05');
-
 -- --------------------------------------------------------
 
 --
@@ -79,13 +54,17 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `user`, `password`, `email`, `img_path`) VALUES
-(4, 'justinn', '$2y$10$8AY0jIa031We0hBZ1zk10OYm5qs3t8zLICG2jaHIF07E3DDiogV5W', 'dsd@gmail.com', 'photo/asparagus-4681835__340.jpg'),
-(9, 'drag', '$2y$10$AkSkXVmd5ltLEXLNhcF5I.AEuRMSXV.9pOeM5xTmTIHduIBkymINu', 'drag@gmail.com', 'photo/saSKEEQKE.jpeg'),
-(4191, 'rohan', '$2y$10$219Z5Bo6FI2BSx/FlD4bt.PENN3qBCLVS1TYBre68XAaESVjdQNGa', 'rohal22@gmail.com', 'photo/artificial-intelligence-7342613__340.webp'),
-(52820522, 'rygel', '$2y$10$2YyVRfNdm8HTWasabbtWA.FPoO2huU2T1dyTBAcN1qg5X3DUseJkm', 'rygetl@gmail.com', 'photo/girl-4663125__340.jpg'),
-(81761352, 'john', '$2y$10$Ha2C/ssEPXRlVXplWYrkl.v4vykA1J./7cE6X3CvSVvUHxUhaviSq', 'john2432@gmail.com', 'photo/pexels-andrea-piacquadio-3778603.jpg'),
-(81761353, 'cristi', '$2y$10$wRc0QqHfwUUz/e7022MJe.OxMk8v8lFBvyPbL1TLx3SLVSsMIl36a', 'cristii22@gmail.com', 'photo/pexels-andrea-piacquadio-943084.jpg'),
-(81761354, 'roja', '$2y$10$Kgsr8CS/dON/ON9ZQLBy4.2nDK3PxHCG2yhxR9sjvDBI3ViPOQ28W', 'roja@gmail.com', 'photo/ai-generated-7751808__340.jpg');
+(1, 'ophia', '$2y$10$KVXnq/yCxOYOmvqnYbI0UeUsg0ZeLaH7e5nxx8Z0Rr7EBvL83SIYG', 'ophia@gmail.com', 'photo/11.jpg'),
+(9, 'Jacob', '$2y$10$avEl.bHL/2vNnIWbgkovuuIYUNBzYADFTxpXPXUf.gViP6Ni65xL2', 'Jacob@gmail.com', 'photo/15.jpg'),
+(20, 'liam', '$2y$10$WkNJS4qYCHiUbXNqRoB4W.8YXpHl3hQ84A8fDfdtEkHMbF5e41qkq', 'laim@gmail.com', 'photo/19.jpg'),
+(63, 'James', '$2y$10$H7Lx2ahI816TS444xIr3v.Pu56HRMPSM66pNk05z0Rte7Ydnx5s0i', 'James@gmail.com', 'photo/2.jpg'),
+(85, 'William', '$2y$10$0Q4RFqO2DpVg36rjrIxVH.R3Jbl7aTH5tO/K/U5nokY6IMpx.GDOu', 'William@gmail.com', 'photo/5.jpg'),
+(838, 'Lucas', '$2y$10$bMBRxYM66TiR2xq2iECobOu5KpAvn.VNIctlK9j1OYmNRAaVvThEm', 'Lucas@gmail.com', 'photo/8.jpg'),
+(5194, 'Ava', '$2y$10$R5w97mOcd/400HzVHd5NteLuNpW4dUlubU/rEc2/qmoCGfLRFotxS', 'ava@gmail.com', 'photo/10.jpg'),
+(71000, 'Sophia', '$2y$10$hFAMNRE9mvBunsBrEHoJtOw6s874fTeFly1bgYXzf2FdRFpgZVH7q', 'Sophia@gmail.com', 'photo/4.jpg'),
+(76994, 'Michael', '$2y$10$2RmRbzQBsFuMqU0C0/qq.uuctnI.o9NCrstqQSJKC3Cx1Vn2FBm9u', 'Michael@gmail.com', 'photo/3..jpg'),
+(178323, 'Loc', '$2y$10$Q/waqe5yLwreh/JJHPgrvuLXQ1UpYSoPsRnN0RT5JSuzLFrTmWB9G', 'Loc@gmail.com', 'photo/9.jpg'),
+(947519, 'David', '$2y$10$U0d.rzDVwxGA9VSMQ83yZ.n2UaB/niEUl51I4eFd3o5NMIXe/cE3.', 'David@gmail.com', 'photo/7.jpg');
 
 --
 -- Indexes for dumped tables
@@ -111,13 +90,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `chat_messages`
 --
 ALTER TABLE `chat_messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=379;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=383;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81761355;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- php_api/registerp.php
+++ php_api/registerp.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   } else {
     $name = testInput($_POST['username']);
     if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-      $response['error_ls'] = "Only letters and white space allowed";
+      $response['error_name'] = "Only letters and white space allowed";
     }
   }
 
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   } else {
     $email = testInput($_POST['email']);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $response['error_email_format'] = "Invalid email format";
+      $response['error_email'] = "Invalid email format";
     }
   }
 
@@ -51,9 +51,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   // Validate and process uploaded image
   if (isset($_FILES["profile_pic"])) {
     $file_tmp = $_FILES["profile_pic"]["tmp_name"];
-    $file_name = $_FILES["profile_pic"]["name"];
-    $file_path =  "../photo/" . $file_name;
-    $file_path2 =  "photo/" . $file_name;
+    $file_name = basename($_FILES["profile_pic"]["name"]);
+    $file_path = "../photo/" . $file_name;
+    $file_path2 = "photo/" . $file_name;
 
     $check = getimagesize($file_tmp);
     $imageFileType = strtolower(pathinfo($file_path, PATHINFO_EXTENSION));
@@ -65,17 +65,17 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     if (file_exists($file_path)) {
       $uploadOK = 0;
-      $response['error_profile_pic_exists'] = "Error: File already exists";
+      $response['error_profile_pic'] = "Error: File already exists";
     }
 
     if ($_FILES["profile_pic"]["size"] > 500000) {
       $uploadOK = 0;
-      $response['error_profile_pic_size'] = "Error: File size is too large";
+      $response['error_profile_pic'] = "Error: File size is too large";
     }
 
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+    if (!in_array($imageFileType, array("jpg", "jpeg", "png", "gif"))) {
       $uploadOK = 0;
-      $response['error_profile_pic_type'] = "Error: Only JPG, JPEG, PNG, and GIF files are allowed";
+      $response['error_profile_pic'] = "Error: Only JPG, JPEG, PNG, and GIF files are allowed";
     }
   } else {
     $response['error_profile_pic'] = "Error: Profile picture is required";
@@ -84,17 +84,24 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
 // Insert data into database if all validations are passed
 if ($uploadOK == 1 && empty($response) && isset($file_path) && isset($name) && isset($email) && isset($pass)) {
-  // Check if username or email already exists in the database
-  $query = "SELECT * FROM `user` WHERE `user`='$name' OR `email`='$email'";
-  $result = mysqli_query($conn, $query);
+  // Check if username or email already exists in the database using prepared statements
+  $query = "SELECT * FROM `user` WHERE `user`=? OR `email`=?";
+  $stmt = mysqli_prepare($conn, $query);
+  mysqli_stmt_bind_param($stmt, "ss", $name, $email);
+  mysqli_stmt_execute($stmt);
+  $result = mysqli_stmt_get_result($stmt);
+
   if (mysqli_num_rows($result) > 0) {
     $response['error_exists'] = "Username or email already exists";
   } else {
     $secure_pass = password_hash($pass, PASSWORD_DEFAULT);
 
-    $insert_query = "INSERT INTO `user` (`id`, `user`, `password`, `email`, `img_path`) VALUES (UUID(), '$name', '$secure_pass', '$email', '$file_path2');";
+    $insert_query = "INSERT INTO `user` (`id`, `user`, `password`, `email`, `img_path`) VALUES (UUID(), ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $insert_query);
+    mysqli_stmt_bind_param($stmt, "ssss", $name, $secure_pass, $email, $file_path2);
+    mysqli_stmt_execute($stmt);
+
     move_uploaded_file($file_tmp, $file_path);
-    mysqli_query($conn, $insert_query);
 
     $response['success'] = true;
     $response['message'] = "Registration successful";
